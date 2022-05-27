a7ee4078aeefdd7d77fa136222162ef8a88b1cb2
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-05-27 22:23:58 +0530

    feat: add custom permissions based on user type

---
+++ user/permissions.py
@@ -0,0 +1,45 @@
+from rest_framework import permissions
+
+
+class UserIsAdmin(permissions.BasePermission):
+    '''
+        permission check if the user is Admin
+    '''
+
+    def has_permission(self, request, view):
+        if request.user.is_admin:
+            return True
+        return False
+
+
+class UserIsPrincipal(permissions.BasePermission):
+    '''
+        permission check if the user is Principal
+    '''
+
+    def has_permission(self, request, view):
+        if request.user.is_principal:
+            return True
+        return False
+
+
+class UserIsHOD(permissions.BasePermission):
+    '''
+        permission check if the user is HOD
+    '''
+
+    def has_permission(self, request, view):
+        if request.user.is_hod:
+            return True
+        return False
+
+
+class UserIsTeacher(permissions.BasePermission):
+    '''
+        permission check if the user is Teacher
+    '''
+
+    def has_permission(self, request, view):
+        if request.user.is_teacher:
+            return True
+        return False
