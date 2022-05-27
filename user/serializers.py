2abd8a3172c13e771ac237164cb9b4fcf22b6dd2
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-05-27 22:23:33 +0530

    feat: add serializers for listing and creating user

---
+++ user/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    '''
+        Serializer for User List and Detail
+    '''
+
+    class Meta:
+        model = User
+        exclude = [
+            'groups',
+            'password',
+            'last_login',
+            'user_permissions',
+        ]
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    '''
+        Serializer for User Creation, Update
+    '''
+    password = serializers.CharField(min_length=8,
+                                     max_length=16,
+                                     validators=[validate_password])
+    confirm_password = serializers.CharField(min_length=8,
+                                             max_length=16,
+                                             validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'gender',
+            'password',
+            'confirm_password',
+        ]
