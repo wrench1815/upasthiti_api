9a52ade332c43d758168e0fc7ef45b0bc235717f
preitychib <preeti.chib2015@gmail.com>
2022-06-03 12:45:20 +0530

    added teacher models

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class TeacherModel(models.Model):
+    """Model definition for TeacherModel."""
+
+    teacher_name = models.TextField()
+    teacher_department = models.IntegerField()
+
+    class Meta:
+        """Meta definition for TeacherModel."""
+
+        verbose_name = 'teacher'
+        verbose_name_plural = 'teachers'
+
+    def __str__(self):
+        """Unicode representation of TeacherModel."""
+        return self.teacher_name
