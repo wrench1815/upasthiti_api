59ef6f175f530669cb68c82044ed14c463c1ac4d
preitychib <preeti.chib2015@gmail.com>
2022-06-11 09:01:22 +0530

    UP-3 Add Serializers for Department

--- department/serializers.py
+++ department/serializers.py
@@ -2,8 +2,28 @@ from rest_framework import serializers
 from . import models
 
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class DepartmentFullSerializer(serializers.ModelSerializer):
+    '''
+        Serializer to display Department Data
+    '''
 
     class Meta:
         model = models.DepartmentModel
         fields = '__all__'
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    '''
+        Serializer to Add and Edit Department Data
+    '''
+
+    class Meta:
+        model = models.DepartmentModel
+        Exclude = [
+            'pk',
+            'created_on',
+        ]
+
+    # Todo : janch partal(validation)
+    def validate(self, attrs):
+        return super().validate(attrs)
