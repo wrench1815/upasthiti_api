20f3c064c62a60b33662c0a4033cfb8baf3c0eac
preitychib <preeti.chib2015@gmail.com>
2022-06-12 17:30:57 +0530

    Added Classes base view of get delete patch

--- classes/views.py
+++ classes/views.py
@@ -56,3 +56,91 @@ class ClassesListCreateAPIView(generics.ListCreateAPIView):
         logger.info(response)
 
         return Response(response, status=status.HTTP_201_CREATED)
+
+
+class ClassesRetrieveUpdateDestroyAPIView(generics.GenericAPIView):
+    '''
+        Allowed methods: GET, PATCH, DELETE
+
+        GET: Return Classes of given Id
+        PATCH: Update Classes of given Id with Validated data provided
+        DELETE: Delete Classes of given Id
+
+        Note: Updatation on Classes is done via Partial Update method
+
+        args: pk
+        
+        Accessible by: Admin, HOD, Teacher
+    '''
+    queryset = models.ClassesModel.objects.all()
+    serializer_class = serializers.ClassesSerializer
+    permission_classes = [
+        permissions.IsAuthenticated & (UserIsAdmin | UserIsHOD|UserIsTeacher)
+    ]
+    lookup_field = 'pk'
+
+    #? get single Class(s)
+    @extend_schema(
+        description=
+        'Returns Single User registered on Application of given Id.\n\nargs: pk\n\nAccessible by: Admin, HOD, Teacher',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            serializers.ClassesFullSerializer,
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(description='Not found')
+        })
+    def get(self, request, *args, **kwargs):
+        classes = self.get_object()
+        serializer = serializers.ClassesFullSerializer(classes)
+        return Response(serializer.data)
+
+    #? Update Classes of given Id
+    @extend_schema(
+        request=serializers.ClassesSerializer,
+        description=
+        'Updates the Classes of given Id with the provided Data.\n\nargs: pk\n\nAccessible by: Admin, HOD, Teacher',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(description='Classes Updated Successfully'),
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(description='Not found')
+        })
+    def patch(self, request, *args, **kwargs):
+        classes= self.get_object()
+        serializer = serializers.ClassesSerializer(
+            classes,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        response = {'detail': 'Classes Updated Sucecssfully'}
+        logger.info(response)
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    #? Delete Classes of given Id
+    @extend_schema(
+        description=
+        'Deletes the Classes of the given Id.\n\nargs: pk\n\nAccessible by: Admin, HOD, Teacher',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(description='Classes Deleted Successfully'),
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(description='Not found')
+        })
+    def delete(self, request, *args, **kwargs):
+        classes = self.get_object()
+        classes.delete()
+
+        response = {'detail': 'Classes Deleted Successfully'}
+        logger.info(response)
+
+        return Response(response, status=status.HTTP_200_OK)
