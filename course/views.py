65905cb81d9423da15f977419a54920296eace1c
preitychib <preeti.chib2015@gmail.com>
2022-06-12 18:07:16 +0530

    Added Course get patch delete base view

--- course/views.py
+++ course/views.py
@@ -19,7 +19,7 @@ class CourseListCreateAPIView(generics.ListCreateAPIView):
         GET: Returns list of all Courses
         POST: Creates a new Course object
 
-        Accessible by: Admin, HOD, Teacher
+        Accessible by: Admin, Principal, HOD
     '''
     queryset = models.CourseModel.objects.all()
     serializer_class = serializers.CourseFullSerializer
@@ -57,3 +57,92 @@ class CourseListCreateAPIView(generics.ListCreateAPIView):
         logger.info(response)
 
         return Response(response, status=status.HTTP_201_CREATED)
+
+
+class CourseRetrieveUpdateDestroyAPIView(generics.GenericAPIView):
+    '''
+        Allowed methods: GET, PATCH, DELETE
+
+        GET: Return Course of given Id
+        PATCH: Update Course of given Id with Validated data provided
+        DELETE: Delete Course of given Id
+
+        Note: Updatation on Course is done via Partial Update method
+
+        args: pk
+        
+        Accessible by: Admin,Principal, HOD
+    '''
+    queryset = models.CourseModel.objects.all()
+    serializer_class = serializers.CourseSerializer
+    permission_classes = [
+        permissions.IsAuthenticated &
+        (UserIsAdmin | UserIsHOD | UserIsPrincipal)
+    ]
+    lookup_field = 'pk'
+
+    #? get single Course
+    @extend_schema(
+        description=
+        'Returns Single Course on Application of given Id.\n\nargs: pk\n\nAccessible by: Admin',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            serializers.CourseFullSerializer,
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(description='Not found')
+        })
+    def get(self, request, *args, **kwargs):
+        course = self.get_object()
+        serializer = serializers.CourseFullSerializer(course)
+        return Response(serializer.data)
+
+    #? Update Course of given Id
+    @extend_schema(
+        request=serializers.CourseSerializer,
+        description=
+        'Updates the Course of given Id with the provided Data.\n\nargs: pk\n\nAccessible by: Admin',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(description='Course Updated Successfully'),
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(description='Not found')
+        })
+    def patch(self, request, *args, **kwargs):
+        course = self.get_object()
+        serializer = serializers.CourseSerializer(
+            course,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        response = {'detail': 'Course Updated Sucecssfully'}
+        logger.info(response)
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    #? Delete Course of given Id
+    @extend_schema(
+        description=
+        'Deletes the Course of the given Id.\n\nargs: pk\n\nAccessible by: Admin',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(description='Course Deleted Successfully'),
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(description='Not found')
+        })
+    def delete(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.delete()
+
+        response = {'detail': 'Course Deleted Successfully'}
+        logger.info(response)
+
+        return Response(response, status=status.HTTP_200_OK)
