b63876ba11a125dec77df27e83df579b0ba8288e
preitychib <preeti.chib2015@gmail.com>
2022-06-26 15:15:23 +0530

    bulk post with multiple database queries

--- student/views.py
+++ student/views.py
@@ -40,7 +40,7 @@ class StudentListCreateAPIView(generics.ListCreateAPIView):
         },
         description='Creates a new Student Object.')
     def post(self, request, *args, **kwargs):
-        serializer = serializers.StudentSerializer(data=request.data,many=True)
+        serializer = serializers.StudentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         try:
@@ -53,6 +53,52 @@ class StudentListCreateAPIView(generics.ListCreateAPIView):
                             status=status.HTTP_400_BAD_REQUEST)
 
         response = {'detail': 'Student Added Successfully'}
+        logger.info(response)
+
+        return Response(response, status=status.HTTP_201_CREATED)
+
+
+class StudentBulkCreateAPIView(generics.GenericAPIView):
+    '''
+        Allowed methods: Bulk Post
+
+        
+        POST: Creates a new Student object
+
+        Accessible by: Admin, HOD, Teacher
+    '''
+    queryset = models.StudentModel.objects.all()
+    serializer_class = serializers.StudentFullSerializer
+    permission_classes = [
+        permissions.IsAuthenticated & (UserIsAdmin | UserIsHOD | UserIsTeacher)
+    ]
+
+    #? Bulk Create Student Objects
+    @extend_schema(
+        request=serializers.StudentSerializer,
+        responses={
+            #? 201
+            status.HTTP_201_CREATED:
+            OpenApiResponse(description='Students Added Successfully'),
+            #? 400
+            status.HTTP_400_BAD_REQUEST:
+            OpenApiResponse({})
+        },
+        description='Bulk Create Student Objects.')
+    def post(self, request, *args, **kwargs):
+        serializer = serializers.StudentSerializer(data=request.data,many=True)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            serializer.save()
+
+        except Exception as ex:
+            logger.error(str(ex))
+
+            return Response({'detail': str(ex)},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        response = {'detail': 'Students Added Successfully'}
         logger.info(response)
 
         return Response(response, status=status.HTTP_201_CREATED)
