901e316d67536b2c040cdbe93617a1340927fa2f
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-07-03 14:28:23 +0530

    feat: add pagination as a seperate object to ease separting pagination data
    add schema for pagination scheme
    update docstrings

--- api/paginator.py
+++ api/paginator.py
@@ -15,13 +15,15 @@ class StandardPagination(PageNumberPagination):
 
         example response:
             {
-                "count": 0,
-                "items": 0,
-                "next": true,
-                "previous": true,
-                "current": 0,
-                "results": [
-                    ...
+                'pagination': {
+                    'count': 0,
+                    'items': 0,
+                    'next': true,
+                    'previous': true,
+                    'current': 0
+                },
+                'results': [
+                    ....
                 ]
             }
     '''
@@ -32,48 +34,59 @@ class StandardPagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
-            # return count of all pages
-            'count': self.page.paginator.num_pages,
+            #? pagination object
+            'pagination': {
+                #? return count of all pages
+                'count': self.page.paginator.num_pages,
 
-            # return counts of all items
-            'items': self.page.paginator.count,
+                #? return counts of all items
+                'items': self.page.paginator.count,
 
-            # return true if there is a next page
-            'next': self.page.has_next(),
+                #? return true if there is a next page
+                'next': self.page.has_next(),
 
-            # returns true if there is a previous page
-            'previous': self.page.has_previous(),
+                #? returns true if there is a previous page
+                'previous': self.page.has_previous(),
 
-            # return current page number
-            'current': self.page.number,
+                #? return current page number
+                'current': self.page.number,
+            },
 
-            # data
+            #? data
             'results': data
         })
 
     def get_paginated_response_schema(self, schema):
+        '''
+            schema for custom paginated response
+        '''
         return {
             'type': 'object',
             'properties': {
-                'count': {
-                    'type': 'integer',
-                    'description': 'Total number of pages'
-                },
-                'items': {
-                    'type': 'integer',
-                    'description': 'Total number of Items',
-                },
-                'next': {
-                    'type': 'boolean',
-                    'description': 'True if there is a next page'
-                },
-                'previous': {
-                    'type': 'boolean',
-                    'description': 'True if there is a previous page'
-                },
-                'current': {
-                    'type': 'integer',
-                    'description': 'Current page number'
+                'pagination': {
+                    'type': 'object',
+                    'properties': {
+                        'count': {
+                            'type': 'integer',
+                            'description': 'Total number of pages'
+                        },
+                        'items': {
+                            'type': 'integer',
+                            'description': 'Total number of Items',
+                        },
+                        'next': {
+                            'type': 'boolean',
+                            'description': 'True if there is a next page'
+                        },
+                        'previous': {
+                            'type': 'boolean',
+                            'description': 'True if there is a previous page'
+                        },
+                        'current': {
+                            'type': 'integer',
+                            'description': 'Current page number'
+                        },
+                    },
                 },
                 'results': schema
             }
