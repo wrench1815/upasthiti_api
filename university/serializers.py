fe11d88f8c75e4785c2dee7d8624914ea4509bb0
wrench1815 <hardeepkumar1815@gmail.com>
2022-08-09 10:58:44 +0530

    feat: add model methods that return - total university count - list of all colleges affiliated to the university
    update serializers to
    - add nested serilizer for college
    - include model methods in serializer

--- university/serializers.py
+++ university/serializers.py
@@ -1,6 +1,29 @@
 from rest_framework import serializers
 
 from .models import UniversityModel
+from college.models import CollegeModel
+from college import serializers as college_serializers
+
+
+##############################################################################################
+# Start:Nested Serialisers
+##############################################################################################
+class UniCollegeSerializer(serializers.ModelSerializer):
+    '''
+        Nested CollegeSerializer for University
+    '''
+
+    hod = college_serializers.CollegeHODSerializer(many=True)
+    principal = college_serializers.CollegePrincipalSerializer()
+
+    class Meta:
+        model = CollegeModel
+        exclude = ['university']
+
+
+##############################################################################################
+# End:Nested Serialisers
+##############################################################################################
 
 
 class UniversitySerializer(serializers.ModelSerializer):
@@ -8,9 +31,29 @@ class UniversitySerializer(serializers.ModelSerializer):
         Serializer for LIsting University Data
     '''
 
+    college_affiliated = UniCollegeSerializer(
+        source='get_college_affiliated',
+        many=True,
+    )
+    college_affiliated_count = serializers.IntegerField(
+        source='get_college_affiliated_count')
+
     class Meta:
         model = UniversityModel
-        fields = '__all__'
+        fields = [
+            'name',
+            'address',
+            'alias',
+            'district',
+            'email',
+            'phone_number',
+            'logo',
+            'logo_public_id',
+            'vice_chancelor',
+            'date_added',
+            'college_affiliated',
+            'college_affiliated_count',
+        ]
 
 
 class UniversityCreateSerializer(serializers.ModelSerializer):
