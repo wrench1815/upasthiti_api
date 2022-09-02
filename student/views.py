9fa2935768e08abb3afff0da0ea0a2d2b0eea70d
wrench1815 <hardeepkumar1815@gmail.com>
2022-09-02 12:19:11 +0530

    👌 IMPROVE: update View Docs

--- student/views.py
+++ student/views.py
@@ -1,17 +1,62 @@
 import logging
 
-from drf_spectacular.utils import OpenApiResponse, extend_schema
+from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
+from drf_spectacular.types import OpenApiTypes
 
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+from rest_framework.filters import OrderingFilter
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from django.conf import settings
 
 from . import serializers, models
 
 from user.permissions import UserIsAdmin, UserIsHOD, UserIsTeacher
 
+from api.paginator import StandardPagination
+
 logger = logging.getLogger(__name__)
 
 
+@extend_schema_view(
+    post=extend_schema(
+        request=serializers.StudentSerializer,
+        responses={
+            #? 201
+            status.HTTP_201_CREATED:
+            OpenApiResponse(description='Student Added Successfully'),
+            #? 400
+            status.HTTP_400_BAD_REQUEST:
+            OpenApiResponse(
+                description='Bad Request',
+                response=OpenApiTypes.OBJECT,
+            )
+        },
+        description=
+        'Allowed methods: GET, POST \n\nPOST: Creates a new Student object\n\nAccessible by: Admin, HOD, Teacher',
+    ),
+    get=extend_schema(
+        request=serializers.StudentFullSerializer,
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(
+                description='Student List',
+                response=serializers.StudentFullSerializer,
+            ),
+            #? 400
+            status.HTTP_400_BAD_REQUEST:
+            OpenApiResponse(
+                description='Bad Request',
+                response=OpenApiTypes.OBJECT,
+            ),
+        },
+        description=
+        'Returns list of all Students.\n\nOrdering:\n\n- default: -created_on\n\n- allowed: created_on, -created_on'
+    ),
+)
 class StudentListCreateAPIView(generics.ListCreateAPIView):
     '''
         Allowed methods: GET, POST
@@ -26,21 +71,12 @@ class StudentListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = [
         permissions.IsAuthenticated & (UserIsAdmin | UserIsHOD | UserIsTeacher)
     ]
+    pagination_class = StandardPagination
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['created_on']
+    ordering = '-created_on'
 
     #? create a new Student Object
-    @extend_schema(
-        request=serializers.StudentSerializer,
-        responses={
-            #? 201
-            status.HTTP_201_CREATED:
-            OpenApiResponse(description='Student Added Successfully'),
-            #? 400
-            status.HTTP_400_BAD_REQUEST:
-            OpenApiResponse({})
-        },
-        description=
-        'Allowed methods: GET, POST \n\nPOST: Creates a new Student object\n\nAccessible by: Admin, HOD, Teacher',
-    )
     def post(self, request, *args, **kwargs):
         serializer = serializers.StudentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -48,18 +84,91 @@ class StudentListCreateAPIView(generics.ListCreateAPIView):
         try:
             serializer.save()
 
+            #! Runs only if DEBUG = True
+            if settings.DEBUG:
+                logger.info(serializer.data)
+
         except Exception as ex:
             logger.error(str(ex))
 
             return Response({'detail': str(ex)},
                             status=status.HTTP_400_BAD_REQUEST)
 
-        response = {'detail': 'Student Added Successfully'}
-        logger.info(response)
+        response = serializer.data
+        logger.info('Student Added Successfully')
 
         return Response(response, status=status.HTTP_201_CREATED)
 
 
+@extend_schema_view(
+    get=extend_schema(
+        description=
+        'Returns Single Student on Application of given Id.\n\nargs: pk\n\nAccessible by: Admin, Teacher',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(
+                description='Student Details',
+                response=serializers.StudentFullSerializer,
+            ),
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(
+                description='Not found',
+                response=OpenApiTypes.OBJECT,
+            ),
+            #? 400
+            status.HTTP_400_BAD_REQUEST:
+            OpenApiResponse(
+                description='Bad Request',
+                response=OpenApiTypes.OBJECT,
+            ),
+        }),
+    patch=extend_schema(
+        request=serializers.StudentSerializer,
+        description=
+        'Updates the Student of given Id with the provided Data.\n\nargs: pk\n\nAccessible by: Admin, Teacher',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(
+                description='Student Updated Successfully',
+                response=OpenApiTypes.OBJECT,
+            ),
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(
+                description='Not found',
+                response=OpenApiTypes.OBJECT,
+            ),
+            #? 400
+            status.HTTP_400_BAD_REQUEST:
+            OpenApiResponse(
+                description='Bad Request',
+                response=OpenApiTypes.OBJECT,
+            ),
+        }),
+    delete=extend_schema(
+        description=
+        'Deletes the Student of the given Id.\n\nargs: pk\n\nAccessible by: Admin, Teacher',
+        responses={
+            #? 200
+            status.HTTP_200_OK:
+            OpenApiResponse(description='Student Deleted Successfully', ),
+            #? 404
+            status.HTTP_404_NOT_FOUND:
+            OpenApiResponse(
+                description='Not found',
+                response=OpenApiTypes.OBJECT,
+            ),
+            #? 400
+            status.HTTP_400_BAD_REQUEST:
+            OpenApiResponse(
+                description='Bad Request',
+                response=OpenApiTypes.OBJECT,
+            ),
+        }),
+)
 class StudentRetrieveUpdateDestroyAPIView(generics.GenericAPIView):
     '''
         Allowed methods: GET, PATCH, DELETE
@@ -82,35 +191,12 @@ class StudentRetrieveUpdateDestroyAPIView(generics.GenericAPIView):
     lookup_field = 'pk'
 
     #? get single Student
-    @extend_schema(
-        description=
-        'Returns Single Student on Application of given Id.\n\nargs: pk\n\nAccessible by: Admin, Teacher',
-        responses={
-            #? 200
-            status.HTTP_200_OK:
-            serializers.StudentFullSerializer,
-            #? 404
-            status.HTTP_404_NOT_FOUND:
-            OpenApiResponse(description='Not found')
-        })
     def get(self, request, *args, **kwargs):
         student = self.get_object()
         serializer = serializers.StudentFullSerializer(student)
         return Response(serializer.data)
 
     #? Update Student of given Id
-    @extend_schema(
-        request=serializers.StudentSerializer,
-        description=
-        'Updates the Student of given Id with the provided Data.\n\nargs: pk\n\nAccessible by: Admin, Teacher',
-        responses={
-            #? 200
-            status.HTTP_200_OK:
-            OpenApiResponse(description='Student Updated Successfully'),
-            #? 404
-            status.HTTP_404_NOT_FOUND:
-            OpenApiResponse(description='Not found')
-        })
     def patch(self, request, *args, **kwargs):
         student = self.get_object()
         serializer = serializers.StudentSerializer(
@@ -127,17 +213,6 @@ class StudentRetrieveUpdateDestroyAPIView(generics.GenericAPIView):
         return Response(response, status=status.HTTP_200_OK)
 
     #? Delete Student of given Id
-    @extend_schema(
-        description=
-        'Deletes the Student of the given Id.\n\nargs: pk\n\nAccessible by: Admin, Teacher',
-        responses={
-            #? 200
-            status.HTTP_200_OK:
-            OpenApiResponse(description='Student Deleted Successfully'),
-            #? 404
-            status.HTTP_404_NOT_FOUND:
-            OpenApiResponse(description='Not found')
-        })
     def delete(self, request, *args, **kwargs):
         student = self.get_object()
         student.delete()
